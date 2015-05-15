e92cba87db9fccd7453b91308bfdcc4215365bc7
Arek <t8534@o2.pl>
2015-05-15 20:09:29 +0200

    [master] Update to Blinky LED test. Working. Ready for base Tag. And than to fork to development and devel_make branches.

--- bc_1114/src/bc_1114.c
+++ bc_1114/src/bc_1114.c
@@ -1,11 +1,16 @@
 /*
 ===============================================================================
  Name        : bc_1114.c
+
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
+
+Blinky example using timers and sysTick.
+This is base template for future changes.
+
 */
 
 #if defined (__USE_LPCOPEN)
@@ -18,12 +23,54 @@
 
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
 
-// TODO: insert other definitions and declarations here
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+
+#define TICKRATE_HZ1 (10) /* 10 ticks per second */
+#define TICKRATE_HZ2 (11) /* 11 ticks per second */
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+
+/**
+ * @brief	Handle interrupt from SysTick timer
+ * @return	Nothing
+ */
+void SysTick_Handler(void)
+{
+	Board_LED_Set(0, false);
+}
+
+/**
+ * @brief	Handle interrupt from 32-bit timer
+ * @return	Nothing
+ */
+void TIMER32_0_IRQHandler(void)
+{
+	if (Chip_TIMER_MatchPending(LPC_TIMER32_0, 1)) {
+		Chip_TIMER_ClearMatch(LPC_TIMER32_0, 1);
+		Board_LED_Set(0, true);
+	}
+}
+
 
 int main(void) {
 
+#if 0
+
+// Old example, remove after blinky will work
 #if defined (__USE_LPCOPEN)
 #if !defined(NO_BOARD_LIB)
     // Read clock settings and update SystemCoreClock variable
@@ -45,4 +92,41 @@ int main(void) {
         i++ ;
     }
     return 0 ;
+
+#endif
+
+
+	uint32_t timerFreq;
+
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Enable and setup SysTick Timer at a periodic rate */
+	SysTick_Config(SystemCoreClock / TICKRATE_HZ1);
+
+	/* Enable timer 1 clock */
+	Chip_TIMER_Init(LPC_TIMER32_0);
+
+	/* Timer rate is system clock rate */
+	timerFreq = Chip_Clock_GetSystemClockRate();
+
+	/* Timer setup for match and interrupt at TICKRATE_HZ */
+	Chip_TIMER_Reset(LPC_TIMER32_0);
+	Chip_TIMER_MatchEnableInt(LPC_TIMER32_0, 1);
+	Chip_TIMER_SetMatch(LPC_TIMER32_0, 1, (timerFreq / TICKRATE_HZ2));
+	Chip_TIMER_ResetOnMatchEnable(LPC_TIMER32_0, 1);
+	Chip_TIMER_Enable(LPC_TIMER32_0);
+
+	/* Enable timer interrupt */
+	NVIC_ClearPendingIRQ(TIMER_32_0_IRQn);
+	NVIC_EnableIRQ(TIMER_32_0_IRQn);
+
+	/* LEDs toggle in interrupt handlers */
+	while (1) {
+		__WFI();
+	}
+
+	return 0;
+
+
 }
